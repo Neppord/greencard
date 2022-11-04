c7cedb2673b6da5f53492d1274bb89c0643346b5
neppord <neppord@gmail.com>
2022-11-04 16:27:57 +0100

    Add some data model to represent plants

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,32 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+data Growth = Dead | Growth Int
+data PlantState = Germenation | Flower | Fruit
+
+type CardEffect =  Plant -> Plant
+
+type Card =
+    { growth :: Growth
+    , effect :: CardEffect
+    }
+
+
+newtype Seed = Seed
+    { germenation :: Int
+    , flower :: Int
+    , fruit :: Int
+    }
+
+newtype Plant = Plant
+    { cards :: Array Card
+    , genome :: Array Card
+    , seed :: Seed
+    , state :: PlantState
+    , timeLeft :: Int
+    }
+
+
 main :: Effect Unit
 main = do
   log "🍝"
