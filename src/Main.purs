7abbd8518fd80d2494ae97db1d70076a18fd183a
neppord <neppord@gmail.com>
2022-11-06 19:03:56 +0100

    Run simulation for 20 days

--- src/Main.purs
+++ src/Main.purs
@@ -3,11 +3,12 @@ module Main where
 import Prelude
 
 import Cards (Card(..), basicGrowth, basicPrice, basicSeeds)
-import Data.Array (length, mapMaybe, replicate, sortWith, uncons, zip)
+import Data.Array (foldl, length, mapMaybe, partition, replicate, sortWith, uncons, zip, (..))
+import Data.Foldable (sum)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
-import Data.Traversable (for)
+import Data.Traversable (for, sequence)
 import Data.Tuple (fst, snd)
 import Effect (Effect)
 import Effect.Console (log)
@@ -34,19 +35,6 @@ derive instance Generic Plant _
 instance Show Plant where
     show (Plant p) = show p.stats
 
-baseSeed :: Seed
-baseSeed = Seed
-    { daysToHarvest: 3
-    , genome:
-        [ basicGrowth
-        , basicGrowth
-        , basicGrowth
-        , basicSeeds
-        , basicPrice
-        ]
-    , stats: Stats { growth: 0, price: 0, seeds: 0}
-    }
-
 
 shuffle :: forall a. Array a -> Effect (Array a)
 shuffle a = do
@@ -72,20 +60,90 @@ drawCard (Plant p) = case uncons p.cards of
         , stats = p.stats + card.stats
         }
 
-tick :: Array Plant -> Effect (Array Plant)
-tick plants = do
-    log $ show plants
-    pure $ mapMaybe drawCard plants
+shouldHarvest :: Plant -> Boolean
+shouldHarvest (Plant {stats: (Stats {growth}), daysToHarvest}) =
+    growth >= daysToHarvest
+
+newtype Player = Player
+    { plants :: Array Plant
+    , seeds :: Array Seed
+    , money :: Int
+    }
+instance Show Player where
+    show (Player player) =
+        "Money: " <> show player.money <> "\n"
+        <> "plants: " <> show (length player.plants) <> "\n"
+        <> "seeds: " <> show (length player.seeds)
+
+agePlants :: Player -> Player
+agePlants (Player player) = Player $ player
+    { plants = mapMaybe drawCard player.plants
+    }
+
+harvestPlants :: Player -> Player
+harvestPlants (Player player) = Player $ player
+    { plants = harvested.no
+    , money = player.money + revenue
+    , seeds = player.seeds <> seeds
+    }
+    where
+        harvested = partition shouldHarvest player.plants
+        revenue = harvested.yes
+            # map (\(Plant {stats}) -> stats)
+            # map (\(Stats {price}) -> price)
+            # sum
+        seeds = harvested.yes
+            # map (\(Plant p) -> p.seed)
+
+plantSeeds :: Player -> Effect Player
+plantSeeds (Player player) = do
+    plants <- sequence $ plant <$> player.seeds
+    pure $ Player $ player
+        { plants = player.plants <> plants
+        , seeds = []
+        }
+
+tick :: Player -> Effect Player
+tick player = do
+    player' <- plantSeeds player
+    pure $ harvestPlants $ agePlants player'
+
+baseSeed :: Seed
+baseSeed = Seed
+    { daysToHarvest: 3
+    , genome:
+        [ basicGrowth
+        , basicGrowth
+        , basicGrowth
+        , basicSeeds
+        , basicPrice
+        ]
+    , stats: Stats { growth: 0, price: 0, seeds: 0}
+    }
+
+start :: Player
+start = Player
+    { plants: []
+    , seeds: [baseSeed, baseSeed, baseSeed]
+    , money: 0
+    }
+
+doX :: forall a. Int -> (Int -> a -> Effect a) -> a -> Effect a
+doX x f = chain ticks
+    where
+        chain :: Array (a -> Effect a) -> a -> Effect a
+        chain = foldl (>=>) pure
+        ticks :: Array (a -> Effect a)
+        ticks =  f <$> 0..x
 
 main :: Effect Unit
 main = do
-    p <- plant baseSeed
-    log "day 0"
-    newPlants <- tick [ p ]
+    let numberOfDays = 20
+    player <- start # doX numberOfDays \ d player -> do
+      log $ "Day " <> show d
+      log $ show player
+      log ""
+      tick player
+    log $ "Day " <> show numberOfDays
+    log $ show player
     log ""
-    log "day 1"
-    newerPlants <- tick newPlants
-    log ""
-    log "day 2"
-    log $ show newerPlants
-    pure unit
